ce9389bebfb8f1f279d3c9966800c0ac70da65c9
Onkar Bartakke <onkarbartakke27136@gmail.com>
2024-06-23 10:19:31 +0530

    initial-setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
+ 
